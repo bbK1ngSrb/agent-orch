608213cd2aae60d0ce7b68cb56f15f962a90b24c
Boris Milinkovic <github@milinkovic.info>
2026-07-04 11:45:42 +0200

    Add copilot adapter

---
+++ src/adapters/copilot.js
@@ -0,0 +1,9 @@
+import { makeCliAdapter } from "./cli-adapter.js";
+
+export function buildArgs(prompt, wd, opts = {}) {
+  const args = ["-p", prompt, "--allow-all-tools", "--add-dir", wd];
+  if (opts.model) args.push("--model", opts.model);
+  return args;
+}
+
+export default makeCliAdapter({ name: "copilot", bin: "copilot", buildArgs });
